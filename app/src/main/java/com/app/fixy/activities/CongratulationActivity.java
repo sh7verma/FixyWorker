8bb5a25aef4795305a60da69461fc4b44869fdfa
Sunny <spdeol20@gmail.com>
2018-08-16 23:07:45 +0530

    congratulation

---
+++ app/src/main/java/com/app/fixy/activities/CongratulationActivity.java
@@ -0,0 +1,42 @@
+package com.app.fixy.activities;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.app.fixy.R;
+
+public class CongratulationActivity extends BaseActivity {
+
+
+    @Override
+    protected int getContentView() {
+        return R.layout.activity_congratulation;
+    }
+
+    @Override
+    protected void onCreateStuff() {
+
+    }
+
+    @Override
+    protected void initUI() {
+
+    }
+
+    @Override
+    protected void initListener() {
+
+    }
+
+    @Override
+    protected Context getContext() {
+        return this;
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+}
