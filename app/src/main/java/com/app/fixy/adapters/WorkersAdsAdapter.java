4924a5850e770835dec8432660f890b166fe1cb3
sh7verma <shubham.verma740@gmail.com>
2018-08-18 14:01:05 +0530

    18-08-2018 02:03 PM

--- app/src/main/java/com/app/fixy/adapters/WorkersAdsAdapter.java
+++ app/src/main/java/com/app/fixy/adapters/WorkersAdsAdapter.java
@@ -1,19 +1,14 @@
 package com.app.fixy.adapters;
 
 import android.content.Context;
-import android.graphics.Color;
-import android.graphics.drawable.GradientDrawable;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
 
 import com.app.fixy.R;
 
-import butterknife.BindView;
 import butterknife.ButterKnife;
 
 /**
