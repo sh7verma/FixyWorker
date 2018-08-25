769fac2f967b0859d1146fbaaa77dcbda5618abf
sh7verma <shubham.verma740@gmail.com>
2018-08-25 21:34:57 +0530

    25-08-2018 PayU Lib Added

--- app/src/main/java/com/app/fixy/fragments/ProfileFragment.java
+++ app/src/main/java/com/app/fixy/fragments/ProfileFragment.java
@@ -7,7 +7,7 @@ import android.view.View;
 import android.widget.LinearLayout;
 
 import com.app.fixy.R;
-import com.app.fixy.activities.SearchAddressActivity;
+import com.app.fixy.activities.SettingsActivity;
 import com.app.fixy.activities.ShowAddressActivity;
 
 import butterknife.BindView;
@@ -20,8 +20,11 @@ public class ProfileFragment extends BaseFragment {
 
     @SuppressLint("StaticFieldLeak")
     static ProfileFragment fragment;
+
     @BindView(R.id.ll_address)
     LinearLayout llAddress;
+    @BindView(R.id.ll_settings)
+    LinearLayout llSettings;
 
     public static ProfileFragment newInstance(Context mContext) {
         fragment = new ProfileFragment();
@@ -42,15 +45,21 @@ public class ProfileFragment extends BaseFragment {
     @Override
     protected void initListeners() {
         llAddress.setOnClickListener(this);
+        llSettings.setOnClickListener(this);
 
     }
 
     @Override
     public void onClick(View view) {
         Intent intent;
-        switch (view.getId()){
+        switch (view.getId()) {
             case R.id.ll_address:
                 intent = new Intent(getActivity(), ShowAddressActivity.class);
+                startActivity(intent);
+                getActivity().overridePendingTransition(R.anim.in, R.anim.out);
+                break;
+            case R.id.ll_settings:
+                intent = new Intent(getActivity(), SettingsActivity.class);
                 startActivity(intent);
                 getActivity().overridePendingTransition(R.anim.in, R.anim.out);
                 break;
