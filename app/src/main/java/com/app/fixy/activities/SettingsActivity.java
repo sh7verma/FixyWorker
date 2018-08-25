2e49d77b80d09fe805c0bd6bcc7e957dfe8d3e88
sh7verma <shubham.verma740@gmail.com>
2018-08-25 21:28:40 +0530

    25-08-2018 PayU Lib Added

--- app/src/main/java/com/app/fixy/activities/SettingsActivity.java
+++ app/src/main/java/com/app/fixy/activities/SettingsActivity.java
@@ -1,8 +1,6 @@
 package com.app.fixy.activities;
 
 import android.content.Context;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 
 import com.app.fixy.R;
